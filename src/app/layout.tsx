bb9e7f635016707010282a0cb632d9b7f8f523e0
Rutwik Patel <rutwikpatel1313@gmail.com>
2026-01-20 10:45:29 -0800

    Add tracking: Apollo, Vercel Analytics & Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '@/styles/globals.css';
 
 const poppins = Poppins({
@@ -32,7 +35,35 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/myimg/favicon.png" type="image/x-icon" />
       </head>
-      <body className={poppins.variable}>{children}</body>
+      <body className={poppins.variable}>
+        {children}
+
+        {/* Vercel Analytics & Speed Insights */}
+        <Analytics />
+        <SpeedInsights />
+
+        {/* Apollo Tracking Script */}
+        <Script
+          id="apollo-tracker"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              function initApollo(){
+                var n=Math.random().toString(36).substring(7),
+                    o=document.createElement("script");
+                o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n;
+                o.async=true;
+                o.defer=true;
+                o.onload=function(){
+                  window.trackingFunctions.onLoad({appId:"67df2cb5e21e0c001dd10f3e"})
+                };
+                document.head.appendChild(o)
+              }
+              initApollo();
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
